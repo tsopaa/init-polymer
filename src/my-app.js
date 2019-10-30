be1374f1543535ff9232f9530850662bd477b446
Paula <paula.ost123@gmail.com>
2019-10-30 15:54:27 +0100

    kick unnecessary stuff

--- src/my-app.js
+++ src/my-app.js
@@ -1,19 +1,7 @@
 import { PolymerElement, html } from "@polymer/polymer/polymer-element.js";
 import { setPassiveTouchGestures, setRootPath } from "@polymer/polymer/lib/utils/settings.js";
-import "@polymer/app-layout/app-header/app-header.js";
-import "@polymer/app-layout/app-header-layout/app-header-layout.js";
-import "@polymer/app-layout/app-scroll-effects/app-scroll-effects.js";
-import "@polymer/app-layout/app-toolbar/app-toolbar.js";
 import "@polymer/app-route/app-location.js";
 import "@polymer/app-route/app-route.js";
-import "@polymer/iron-pages/iron-pages.js";
-import "@polymer/iron-selector/iron-selector.js";
-import "@polymer/paper-icon-button/paper-icon-button.js";
-import "./my-icons.js";
-import "./my-view1.js";
-import "./my-view2.js";
-import "./my-view3.js";
-import "./my-view404.js";
 
 // Gesture events like tap and track generated from touch will not be
 // preventable, allowing for better scrolling performance.
@@ -33,67 +21,18 @@ class MyApp extends PolymerElement {
 
           display: block;
         }
-
-        app-header {
-          color: #fff;
-          background-color: var(--app-primary-color);
-        }
-
-        app-header paper-icon-button {
-          --paper-icon-button-ink-color: white;
-        }
       </style>
 
       <app-location route="{{route}}" url-space-regex="^[[rootPath]]"> </app-location>
 
       <app-route route="{{route}}" pattern="[[rootPath]]:page" data="{{routeData}}" tail="{{subroute}}"> </app-route>
 
-      <!-- Main content -->
-      <app-header-layout>
-        <app-header slot="header" condenses="" reveals="" effects="waterfall">
-          <app-toolbar>
-            <paper-icon-button icon="my-icons:menu" drawer-toggle=""></paper-icon-button>
-            <div main-title="">My App</div>
-          </app-toolbar>
-        </app-header>
-
-        <iron-pages selected="[[page]]" attr-for-selected="name" role="main">
-          <my-view1 name="view1"></my-view1>
-          <my-view2 name="view2"></my-view2>
-          <my-view3 name="view3"></my-view3>
-          <my-view404 name="view404"></my-view404>
-        </iron-pages>
-      </app-header-layout>
+      <h3>HALLO</h3>
     `;
   }
 
   static get properties() {
-    return {
-      page: {
-        type: String,
-        reflectToAttribute: true
-      },
-      routeData: Object,
-      subroute: Object
-    };
-  }
-
-  static get observers() {
-    return ["_routePageChanged(routeData.page)"];
-  }
-
-  _routePageChanged(page) {
-    // Show the corresponding page according to the route.
-    //
-    // If no page was found in the route data, page will be an empty string.
-    // Show 'view1' in that case. And if the page doesn't exist, show 'view404'.
-    if (!page) {
-      this.page = "view1";
-    } else if (["view1", "view2", "view3"].indexOf(page) !== -1) {
-      this.page = page;
-    } else {
-      this.page = "view404";
-    }
+    return {};
   }
 }
 
